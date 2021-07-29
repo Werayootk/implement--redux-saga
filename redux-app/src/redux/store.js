f3f73c446b26748cb3d68a1c25e075bfad4e1e30
Werayootk <werayoot5800@gmail.com>
2021-07-29 17:17:58 +0700

    05FixBug

--- redux-app/src/redux/store.js
+++ redux-app/src/redux/store.js
@@ -2,7 +2,7 @@ import { createStore, compose } from 'redux';
 import rootReducer from './reducers/index.js';
 
 const store = compose(
-    window.devToolsExtension && window.devToolsExtension(),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
 )(createStore)(rootReducer);
 
 export default store;
